c3ddf3487602b9d3c2cb66599c1337c6661f783b
iuin <826811175@qq.com>
2018-07-24 21:49:26 +0800

    dashboard update

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -7,29 +7,9 @@ import { ClrWizard, ClrWizardPage } from "@clr/angular";
     styleUrls: ['./dashboard.component.scss']
 })
 export class DashBoardComponent implements OnInit {
+
     constructor() { }
 
     ngOnInit(): void { }
-    wizardOpen = true;
 
-    @ViewChild("wizard") wizard: ClrWizard;
-    @ViewChild("pageThree") pageThree: ClrWizardPage;
-    @ViewChild("pageFive") pageFive: ClrWizardPage;
-
-    public jumpTo(page: ClrWizardPage) {
-        if (page && page.completed) {
-            this.wizard.navService.setCurrentPage(page);
-        } else {
-            this.wizard.navService.setLastEnabledPageCurrent();
-        }
-        this.wizard.open();
-    }
-
-    public jumpToThree(): void {
-        this.jumpTo(this.pageThree);
-    }
-
-    public jumpToFive(): void {
-        this.jumpTo(this.pageFive);
-    }
 }
