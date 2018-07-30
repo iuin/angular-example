8e4339479d31bb354187391a4297111efda7fb70
iuin <826811175@qq.com>
2018-07-30 22:56:24 +0800

    finish wizard full screen directive

--- src/app/shared/components/full-screen-wizard/full-screen-wizard.ts
+++ src/app/shared/components/full-screen-wizard/full-screen-wizard.ts
@@ -4,12 +4,9 @@ import { Directive, ElementRef, OnInit, HostBinding, HostListener, Renderer2 } f
     selector: '[fullScreenWizard]',
 })
 export class FullScreenWizardDirective implements OnInit {
-    modalElement: Element;
-    clrWizardOpen = false;
+    private modalElement: Element;
     constructor(el: ElementRef, private renderer: Renderer2) {
-        // el.nativeElement.style.backgroundColor = 'yellow';
         this.modalElement = el.nativeElement;
-
     }
 
     ngOnInit() {
@@ -18,20 +15,51 @@ export class FullScreenWizardDirective implements OnInit {
                 let wizardOpen = mutations[0].target.parentElement.getAttribute('ng-reflect-clr-wizard-open');
                 if (wizardOpen) {
                     let div: HTMLDivElement = <HTMLDivElement>this.modalElement.firstChild.childNodes[1];
-                    div.style.padding = '0px';
-                    let content = <HTMLDivElement> div.firstChild;
-                    content.style.height = '100vh';
-                    content.style.width = '100vh';
-                    let modalContent =  <HTMLElement> (<HTMLCollection> content.getElementsByClassName('modal-content'))[0];
-                    let modalNav =  <HTMLElement>(<HTMLCollection> content.getElementsByClassName('modal-nav'))[0];
-                    modalNav.style.maxHeight = '100vh';
-                    modalContent.style.maxHeight = '100vh';
-                } else {
-                    console.log("open");
+                    if(div) {
+                        let content = <HTMLDivElement>div.firstChild;
+                        let modalHeader = <HTMLElement>(<HTMLCollection>content.getElementsByClassName('modal-header'))[0];
+                        let button: Element = this.renderer.createElement('button');
+                        button.addEventListener('click' , () => {
+                            let shape = button.firstElementChild.getAttribute('shape')
+                            if('window-restore' == shape) {
+                                this.winRestore(div);
+                                button.firstElementChild.setAttribute('shape','window-max');
+                            } else {
+                                this.winMax(div);
+                                button.firstElementChild.setAttribute('shape','window-restore');
+                            }
+                        });
+                        button.classList.add('close');
+                        button.innerHTML = '<clr-icon shape="window-max"></clr-icon>';
+                        this.renderer.setAttribute(button, 'style', 'margin-right:16px');
+                        this.renderer.insertBefore(modalHeader, button, modalHeader.lastElementChild);
+                    }
                 }
             }
         });
         observer.observe(this.modalElement.firstChild, { childList: true });
     }
 
+    winMax(div: HTMLDivElement) {
+        // full scren
+        div.style.padding = '0px';
+        let content = <HTMLDivElement>div.firstChild;
+        content.style.height = '100vh';
+        content.style.width = '100%';
+        let modalContent = <HTMLElement>(<HTMLCollection>content.getElementsByClassName('modal-content'))[0];
+        let modalNav = <HTMLElement>(<HTMLCollection>content.getElementsByClassName('modal-nav'))[0];
+        modalNav.style.maxHeight = '100vh';
+        modalContent.style.maxHeight = '100vh';
+    }
+
+    winRestore(div: HTMLDivElement) {
+        div.style.padding = '0px';
+        let content = <HTMLDivElement>div.firstChild;
+        content.style.height = '75vh';
+        content.style.width = '';
+        let modalContent = <HTMLElement>(<HTMLCollection>content.getElementsByClassName('modal-content'))[0];
+        let modalNav = <HTMLElement>(<HTMLCollection>content.getElementsByClassName('modal-nav'))[0];
+        modalNav.style.maxHeight = '75vh';
+        modalContent.style.maxHeight = '75vh';
+    }
 }
