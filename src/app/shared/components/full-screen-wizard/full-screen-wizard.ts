31fd97eb13cc00d7c8506fc08c8b35f53849c4d1
Fulin <fulin.zhang@emc.com>
2018-07-31 20:08:20 +0800

    fixed issue

--- src/app/shared/components/full-screen-wizard/full-screen-wizard.ts
+++ src/app/shared/components/full-screen-wizard/full-screen-wizard.ts
@@ -5,34 +5,38 @@ import { Directive, ElementRef, OnInit, HostBinding, HostListener, Renderer2 } f
 })
 export class FullScreenWizardDirective implements OnInit {
     private modalElement: Element;
+    private hostTagName: string;
     constructor(el: ElementRef, private renderer: Renderer2) {
         this.modalElement = el.nativeElement;
+        this.hostTagName = this.modalElement.tagName;
     }
 
     ngOnInit() {
         let observer = new MutationObserver((mutations, observe) => {
-            if (mutations.length > 0) {
-                let wizardOpen = mutations[0].target.parentElement.getAttribute('ng-reflect-clr-wizard-open');
+            if (mutations.length > 0 && this.hostTagName === 'CLR-WIZARD') {
+                let wizardOpen = (<Element>mutations[0].target).classList.contains('open');
                 if (wizardOpen) {
-                    let div: HTMLDivElement = <HTMLDivElement>this.modalElement.firstChild.childNodes[1];
-                    if(div) {
-                        let content = <HTMLDivElement>div.firstChild;
-                        let modalHeader = <HTMLElement>(<HTMLCollection>content.getElementsByClassName('modal-header'))[0];
-                        let button: Element = this.renderer.createElement('button');
-                        button.addEventListener('click' , () => {
-                            let shape = button.firstElementChild.getAttribute('shape')
-                            if('window-restore' == shape) {
-                                this.winRestore(div);
-                                button.firstElementChild.setAttribute('shape','window-max');
-                            } else {
-                                this.winMax(div);
-                                button.firstElementChild.setAttribute('shape','window-restore');
-                            }
-                        });
-                        button.classList.add('close');
-                        button.innerHTML = '<clr-icon shape="window-max"></clr-icon>';
-                        this.renderer.setAttribute(button, 'style', 'margin-right:16px');
-                        this.renderer.insertBefore(modalHeader, button, modalHeader.lastElementChild);
+                    if (this.modalElement.firstChild.childNodes.length > 1) {
+                        let div: HTMLDivElement = <HTMLDivElement>this.modalElement.firstChild.childNodes[1];
+                        if (div) {
+                            let content = <HTMLDivElement>div.firstChild;
+                            let modalHeader = <HTMLElement>(<HTMLCollection>content.getElementsByClassName('modal-header'))[0];
+                            let button: Element = this.renderer.createElement('button');
+                            button.classList.add('close');
+                            button.innerHTML = '<clr-icon shape="window-max"></clr-icon>';
+                            button.addEventListener('click', () => {
+                                let shape = button.firstElementChild.getAttribute('shape')
+                                if ('window-restore' === shape) {
+                                    this.winRestore(div);
+                                    button.firstElementChild.setAttribute('shape', 'window-max');
+                                } else {
+                                    this.winMax(div);
+                                    button.firstElementChild.setAttribute('shape', 'window-restore');
+                                }
+                            });
+                            this.renderer.setAttribute(button, 'style', 'margin-right:16px');
+                            this.renderer.insertBefore(modalHeader, button, modalHeader.lastElementChild);
+                        }
                     }
                 }
             }
@@ -40,8 +44,7 @@ export class FullScreenWizardDirective implements OnInit {
         observer.observe(this.modalElement.firstChild, { childList: true });
     }
 
-    winMax(div: HTMLDivElement) {
-        // full scren
+    private winMax(div: HTMLDivElement) {
         div.style.padding = '0px';
         let content = <HTMLDivElement>div.firstChild;
         content.style.height = '100vh';
@@ -52,8 +55,8 @@ export class FullScreenWizardDirective implements OnInit {
         modalContent.style.maxHeight = '100vh';
     }
 
-    winRestore(div: HTMLDivElement) {
-        div.style.padding = '0px';
+    private winRestore(div: HTMLDivElement) {
+        div.style.padding = '';
         let content = <HTMLDivElement>div.firstChild;
         content.style.height = '75vh';
         content.style.width = '';
